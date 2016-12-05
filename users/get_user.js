6401fb43f76cb3bf280cfc99b70d911e1e4c48b9
Cathal Horan <cathal.horan@intercom.io>
2016-12-05 08:35:35 -0500

    Returning only body and not entire response

--- users/get_user.js
+++ users/get_user.js
@@ -2,7 +2,6 @@ var Intercom = require('intercom-client');
 
 var client = new Intercom.Client({ token: process.env['PAT'] });
 var usr_id = process.argv[2];
-console.log(usr_id);
 client.users.find({ user_id: usr_id}, function (rsp){
-  console.log(rsp)
+  console.log(rsp.body)
 });
