e647ffb99ef519d551d5c510f1dd1925c3c288f4
Cathal Horan <cathal.horan@intercom.io>
2017-02-10 05:59:27 -0500

    Changing PAT to AT for env variable. Can use either when running docker cmd but matching name change from personal access token to access token

--- users/get_user.js
+++ users/get_user.js
@@ -1,6 +1,6 @@
 var Intercom = require('intercom-client');
 
-var client = new Intercom.Client({ token: process.env['PAT'] });
+var client = new Intercom.Client({ token: process.env['AT'] });
 var usr_id = process.argv[2];
 client.users.find({ user_id: usr_id}, function (rsp){
   console.log(rsp.body)
